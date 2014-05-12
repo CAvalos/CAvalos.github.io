e15308c13ccdbf64b9f395a51f85f89abc4cda75
CAvalos <cavalos14@smpanthers.org>
2014-05-12 14:04:44 -0700

    toggled buttons

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 function warned() {
-					$("#warning").html("Reading this is potentially dangerous, this will be the biggest waste of time possible.")
-				}
+		$("#warning").html("Reading this is potentially dangerous, this will be the biggest waste of time possible.");
+		$("#warning").toggle();
+		}
 function conclude() {
-					$("#finalconclusion").html(" You will now regret wasting your precious time on this page. Told you. ")
-				}
+		$("#finalconclusion").html(" You will now regret wasting your precious time on this page. Told you. ");
+		$("#finalconclusion").toggle();
+		}
