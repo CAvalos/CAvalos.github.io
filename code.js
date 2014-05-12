babb0771808d9f1b024302e57ba216127d8d6b94
CAvalos <cavalos14@smpanthers.org>
2014-05-12 13:52:54 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,6 @@
+function warned() {
+					$("#warning").html("Reading this is potentially dangerous, this will be the biggest waste of time possible.")
+				}
+function conclude() {
+					$("#finalconclusion").html(" You will now regret wasting your precious time on this page. Told you. ")
+				}
