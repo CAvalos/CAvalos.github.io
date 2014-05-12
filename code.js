3fd198716da498004af25d14f628e2a2bc20b931
CAvalos <cavalos14@smpanthers.org>
2014-05-12 14:06:29 -0700

    untoggled buttons

--- code.js
+++ code.js
@@ -1,8 +1,6 @@
 function warned() {
 		$("#warning").html("Reading this is potentially dangerous, this will be the biggest waste of time possible.");
-		$("#warning").toggle();
 		}
 function conclude() {
 		$("#finalconclusion").html(" You will now regret wasting your precious time on this page. Told you. ");
-		$("#finalconclusion").toggle();
 		}
